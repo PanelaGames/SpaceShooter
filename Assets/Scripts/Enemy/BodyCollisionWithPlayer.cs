fb538798c04f451716082bf33d085627abb299e4
Marcos Garcia <magaum@gmail.com>
2017-11-17 19:03:07 -0200

    Player is destroyed when HP is 0 or less
    Closes #3

---
+++ Assets/Scripts/Enemy/BodyCollisionWithPlayer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodyCollisionWithPlayer : MonoBehaviour
+{
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.CompareTag ("Player")) {
+			other.gameObject.SendMessage ("ApplyDamage", 10f);
+		}
+	}
+}
---
+++ Assets/Scripts/Player/SimpleHull.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleHull : MonoBehaviour
+{
+	public float hitPoints = 200f;
+
+	void LateUpdate ()
+	{
+		if (hitPoints <= 0f) {
+			Destroy (gameObject);
+		}
+	}
+
+	public void ApplyDamage (float amount)
+	{
+		hitPoints -= amount;
+	}
+
+	public void ApplyHealing (float amount)
+	{
+		hitPoints += amount;
+	}
+}
