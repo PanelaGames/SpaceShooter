442c6902d2850a0d13ae6341f90431b7c012fe3c
Marcos Garcia <magaum@gmail.com>
2017-11-23 15:02:53 -0200

    Adjust size based on viewport

--- Assets/Scripts/DestroyWhenOutOfBounds.cs
+++ Assets/Scripts/DestroyWhenOutOfBounds.cs
@@ -4,6 +4,21 @@ using UnityEngine;
 
 public class DestroyWhenOutOfBounds : MonoBehaviour
 {
+	public void Awake ()
+	{
+		Camera mainCamera = Camera.main;
+		Vector2 topLeft = mainCamera.ScreenToWorldPoint (new Vector2 (0, 0));
+		Vector2 bottomRight = mainCamera.ScreenToWorldPoint (
+			                      new Vector2 (
+				                      mainCamera.pixelWidth,
+				                      mainCamera.pixelHeight
+			                      )
+		                      );
+		Debug.Log (topLeft);
+		Debug.Log (bottomRight);
+		transform.localScale = (bottomRight - topLeft) / 2;
+	}
+
 	public void OnTriggerExit2D (Collider2D other)
 	{
 		Destroy (other.gameObject);
