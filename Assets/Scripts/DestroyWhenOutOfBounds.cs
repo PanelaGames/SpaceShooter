3de54f055a4edd5f1ccb5f799d08743821554697
Marcos Garcia <magaum@gmail.com>
2017-11-24 14:43:14 -0200

    Make boundary size dynamic

--- Assets/Scripts/DestroyWhenOutOfBounds.cs
+++ Assets/Scripts/DestroyWhenOutOfBounds.cs
@@ -4,21 +4,28 @@ using UnityEngine;
 
 public class DestroyWhenOutOfBounds : MonoBehaviour
 {
-	public void Awake ()
-	{
-		Camera mainCamera = Camera.main;
-		Vector2 topLeft = mainCamera.ScreenToWorldPoint (new Vector2 (0, 0));
-		Vector2 bottomRight = mainCamera.ScreenToWorldPoint (
-			                      new Vector2 (
-				                      mainCamera.pixelWidth,
-				                      mainCamera.pixelHeight
-			                      )
-		                      );
-		transform.localScale = (bottomRight - topLeft) / 2;
-	}
+    public int padding = 30;
 
-	public void OnTriggerExit2D (Collider2D other)
-	{
-		Destroy (other.gameObject);
-	}
+    public void Awake()
+    {
+        Camera mainCamera = Camera.main;
+        Vector2 topLeft = mainCamera.ScreenToWorldPoint(new Vector2(-padding, -padding));
+        Vector2 bottomRight = mainCamera.ScreenToWorldPoint(
+                                  new Vector2(
+                                      mainCamera.pixelWidth + padding,
+                                      mainCamera.pixelHeight + padding
+                                  )
+                              );
+        Vector2 size = GetComponent<Renderer>().bounds.size;
+        Vector2 rescale = transform.localScale;
+        Vector2 diff = bottomRight - topLeft;
+        rescale.x = diff.x * rescale.x / size.x;
+        rescale.y = diff.y * rescale.y / size.y;
+        transform.localScale = rescale;
+    }
+
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        Destroy(other.gameObject);
+    }
 }
