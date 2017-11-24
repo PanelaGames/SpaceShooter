743c510a45d86670af2647a58fcdc77af10a0dde
Marcos Garcia <magaum@gmail.com>
2017-11-24 20:57:23 -0200

    Add a UI element for the player's hit points
    Closes #1

---
+++ Assets/Scripts/UI/GameInfoController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameInfoController : MonoBehaviour
+{
+    private GameObject player;
+    private SimpleHull playerHull;
+    private float maxHitPoints;
+    private GameObject hitPointsText;
+    private GameObject hitPointsProgress;
+    private float hitPointsProgressMaxWidth;
+    private Vector2 hitPointsProgressInitialScale;
+    public void Awake()
+    {
+        player = GameObject.Find("Player");
+        playerHull = player.GetComponent<SimpleHull>();
+        maxHitPoints = playerHull.hitPoints;
+        hitPointsText = GameObject.Find("Hit points text");
+        hitPointsProgress = GameObject.Find("Hit points progress");
+        hitPointsProgressMaxWidth = hitPointsProgress.GetComponent<RectTransform>().rect.width;
+    }
+    public void LateUpdate()
+    {
+        hitPointsText.GetComponent<Text>().text = playerHull.hitPoints.ToString() + "/" + maxHitPoints.ToString();
+        hitPointsProgress.GetComponent<RectTransform>().sizeDelta = new Vector2(
+            (playerHull.hitPoints / maxHitPoints) * hitPointsProgressMaxWidth,
+            hitPointsProgress.GetComponent<RectTransform>().rect.height
+        );
+    }
+}
