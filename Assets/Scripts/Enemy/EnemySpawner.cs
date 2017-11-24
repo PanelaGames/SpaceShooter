c9fc327a5e3d2268ad5915780b1fa97f1c2f1532
Marcos Garcia <magaum@gmail.com>
2017-11-24 17:30:49 -0200

    Set the next wave spawn time when game starts
    Closes #13

--- Assets/Scripts/Enemy/EnemySpawner.cs
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -4,65 +4,75 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-	public GameObject simpleUfo;
-	public List<GameObject> enemyList = new List<GameObject> ();
+    public GameObject simpleUfo;
+    public List<GameObject> enemyList = new List<GameObject>();
 
-	private float nextWaveSpawnTimer = 2f;
+    private float nextWaveSpawnTimer;
 
-	void Update ()
-	{
-		if (Time.time >= nextWaveSpawnTimer) {
-			switch (Time.frameCount % 3) {
-			case 0:
-				CreateUfoSinCurveWave ();
-				break;
-			case 1:
-				CreateUfoStraightSWave ();
-				break;
-			case 2:
-				CreateRfoSpreadOutWave ();
-				break;
-			}
-			nextWaveSpawnTimer += 2f;
-		}
-	}
+    void Awake()
+    {
+        nextWaveSpawnTimer = Time.time + 2f;
+    }
 
-	private void CreateUfoStraightSWave ()
-	{
-		for (int i = 0; i <= 4; i += 1) {
-			GameObject ufo = Instantiate (
-				                 simpleUfo,
-				                 new Vector2 (14f + 1.25f * i, 3.5f),
-				                 Quaternion.identity
-			                 ) as GameObject;
-			ufo.AddComponent<StraightSMovement> ();
-		}
-	}
+    void Update()
+    {
+        if (Time.time >= nextWaveSpawnTimer)
+        {
+            switch (Time.frameCount % 3)
+            {
+                case 0:
+                    CreateUfoSinCurveWave();
+                    break;
+                case 1:
+                    CreateUfoStraightSWave();
+                    break;
+                case 2:
+                    CreateRfoSpreadOutWave();
+                    break;
+            }
+            nextWaveSpawnTimer += 2f;
+        }
+    }
 
-	private void CreateUfoSinCurveWave ()
-	{
-		for (int i = 0; i <= 4; i += 1) {
-			GameObject ufo = Instantiate (
-				                 simpleUfo,
-				                 new Vector2 (14f + 1.25f * i, 3.5f),
-				                 Quaternion.identity
-			                 ) as GameObject;
-			ufo.AddComponent<SinCurveMovement> ();
-		}
-	}
+    private void CreateUfoStraightSWave()
+    {
+        for (int i = 0; i <= 4; i += 1)
+        {
+            GameObject ufo = Instantiate(
+                simpleUfo,
+                new Vector2(14f + 1.25f * i, 3.5f),
+                Quaternion.identity
+            ) as GameObject;
+            ufo.AddComponent<StraightSMovement>();
+        }
+    }
 
-	private void CreateRfoSpreadOutWave ()
-	{
-		Vector2 bottomLeft = Camera.main.ScreenToWorldPoint (new Vector3 (0, 0, 0));
-		Vector2 topLeft = Camera.main.ScreenToWorldPoint (new Vector3 (0, Screen.height, 0));
-		Vector2 startPosition = new Vector2 (bottomLeft.x, Random.Range (bottomLeft.y, topLeft.y));
-		for (int i = 0; i <= 4; i += 1) {
-			GameObject ufo = Instantiate (
-				                 simpleUfo,
-				                 new Vector2 (14f + 1.25f * i, startPosition.y),
-				                 Quaternion.identity
-			                 ) as GameObject;
-			ufo.AddComponent<SpreadOutMovement> ();
-		}
-	}
+    private void CreateUfoSinCurveWave()
+    {
+        for (int i = 0; i <= 4; i += 1)
+        {
+            GameObject ufo = Instantiate(
+                simpleUfo,
+                new Vector2(14f + 1.25f * i, 3.5f),
+                Quaternion.identity
+            ) as GameObject;
+            ufo.AddComponent<SinCurveMovement>();
+        }
+    }
+
+    private void CreateRfoSpreadOutWave()
+    {
+        Vector2 bottomLeft = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, 0));
+        Vector2 topLeft = Camera.main.ScreenToWorldPoint(new Vector3(0, Screen.height, 0));
+        Vector2 startPosition = new Vector2(bottomLeft.x, Random.Range(bottomLeft.y, topLeft.y));
+        for (int i = 0; i <= 4; i += 1)
+        {
+            GameObject ufo = Instantiate(
+                simpleUfo,
+                new Vector2(14f + 1.25f * i, startPosition.y),
+                Quaternion.identity
+            ) as GameObject;
+            ufo.AddComponent<SpreadOutMovement>();
+        }
+    }
 }
